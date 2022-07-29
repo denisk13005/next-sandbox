73ac09a94c29f276d1c393f804a3bf5035b49f8d
Kirmann Denis <kirmanndenis@gmail.com>
2022-07-29 18:21:32 +0200

    supression home module

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import styles from "../styles/home.module.css";
 
 const Home = () => {
   return (
-    <main className={styles.homeMainContainer}>
+    <main style={{ display: "flex", height: "100vh" }}>
       <Navbar />
       <div style={{ width: "33%", border: "1px solid white" }}>Container</div>
       <div style={{ width: "33%", border: "1px solid white" }}>Right</div>
