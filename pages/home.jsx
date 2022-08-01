ef99d7abb56820c0b2a94fb56426adc57d89eeca
Kirmann Denis <kirmanndenis@gmail.com>
2022-08-01 17:52:50 +0200

    recup des info avec getStaticprops

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,17 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
-const Home = () => {
+const Home = ({ user }) => {
+  console.log(user.data.data);
+
   return (
     <main style={{ height: "100vh", flexGrow: "1" }}>
       {/* <Navbar /> */}
       <div
         style={{ height: "100vh", width: "100%", border: "1px solid white" }}
       >
-        home
+        <h1>User name : {user.data.data.username}</h1>
+        <h2>The name : {user.data.data.name}</h2>
+        <p>id : {user.data.data.id}</p>
       </div>
     </main>
   );
 };
+
+export async function getStaticProps() {
+  const instance = axios.create({
+    // headers : {''}
+  });
+  const { data } = await axios.get(
+    "https://api.twitter.com/2/users/by/username/kryptonSquirrel",
+    {
+      headers: {
+        authorization: `Bearer ${process.env.NEXT_PUBLIC_TOKEN}`,
+      },
+    }
+  );
+  const user = JSON.parse(JSON.stringify({ data }));
+  return {
+    props: {
+      user: user,
+    },
+  };
+}
 
 export default Home;
